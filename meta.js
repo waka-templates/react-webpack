613785c0a10491b6b594aaa741e468b49019d7bb
dwqs <461147874@qq.com>
2017-02-07 11:47:03 +0800

    add meta.js

---
+++ meta.js
@@ -0,0 +1,43 @@
+/**
+ * Created by pomy on 07/02/2017.
+ */
+
+'use strict';
+
+module.exports = {
+    "prompts": {
+        "name"       : {
+            "type"    : "string",
+            "required": true,
+            "message" : "Project name"
+        },
+        "version"     : {
+            "type"    : "string",
+            "message" : "Project version",
+            "default" : "1.0.0"
+        },
+        "description": {
+            "type"    : "string",
+            "required": false,
+            "message" : "Project description",
+            "default" : "A new React project"
+        },
+        "author"     : {
+            "type"   : "string",
+            "message": "Author"
+        },
+        "router": {
+            "type": "confirm",
+            "message": "Need react-router(3.0.2)?"
+        },
+        "test": {
+            "type": "confirm",
+            "message": "Setup unit tests with Jest + Enzyme?"
+        }
+    },
+    "filters":{
+        "test/**/*": "test",
+        "config/test.env.js": "test"
+    },
+    "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\nDocumentation can be found at https://github.com/waka-templates/react-webpack"
+}
